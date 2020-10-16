a7a213cbfccf7f0f0055fd839afe5e866b96a88c
Ledz96 <alessandro.bianchi003@gmail.com>
2020-10-16 17:18:51 +0200

    Action and State with content

--- deliberative/src/template/ActionDeliberative.java
+++ deliberative/src/template/ActionDeliberative.java
@@ -1,4 +1,22 @@
 package template;
 
+import logist.task.Task;
+import logist.topology.Topology;
+
 public class ActionDeliberative {
+    private final Topology.City destination;
+    private final Task pickedUpTask;            // Might be a list of tasks
+
+    public ActionDeliberative(Topology.City destination, Task pickedUpTask) {
+        this.destination = destination;
+        this.pickedUpTask = pickedUpTask;
+    }
+
+    public Topology.City getDestination() {
+        return destination;
+    }
+
+    public Task getPickedUpTask() {
+        return pickedUpTask;
+    }
 }
--- deliberative/src/template/State.java
+++ deliberative/src/template/State.java
@@ -1,4 +1,53 @@
 package template;
 
-public class Status {
+import logist.task.Task;
+import logist.topology.Topology;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class State {
+    private final Topology.City currentCity;
+    private final int currentWeight;
+    private final Set<Task> pickedUpTasks;
+    private final Set<Task> availableTasks;
+
+    public State(Topology.City currentCity, int currentWeight, Set<Task> pickedUpTasks, Set<Task> availableTasks) {
+        this.currentCity = currentCity;
+        this.currentWeight = currentWeight;
+        this.pickedUpTasks = pickedUpTasks;
+        this.availableTasks = availableTasks;
+    }
+
+    public Topology.City getCurrentCity() {
+        return currentCity;
+    }
+
+    public int getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public Set<Task> getPickedUpTasks() {
+        return pickedUpTasks;
+    }
+
+    public Set<Task> getAvailableTasks() {
+        return availableTasks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return currentWeight == state.currentWeight &&
+                currentCity.equals(state.currentCity) &&
+                pickedUpTasks.equals(state.pickedUpTasks) &&
+                availableTasks.equals(state.availableTasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currentCity, currentWeight, pickedUpTasks, availableTasks);
+    }
 }
